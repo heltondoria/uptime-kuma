9b52a3442e1252d0f6b679a3f6768f686fa8faea
helton.costa <helton.costa@g.globo>
2021-12-09 16:27:05 -0300

    Improves the documentation of the database choice process

--- server/database.js
+++ server/database.js
@@ -1,9 +1,12 @@
 let dbType = process.env.DB_DIALECT || "sqlite"
 let Database;
+console.log("Choosing database concrete implementation");
 if (dbType.toLowerCase() === "mysql") {
     Database = require("./database-handler/database-mysql-handler");
+    console.log("Mysql implementation was chosen");
 } else {
     Database = require("./database-handler/database-sqlite-handler");
+    console.log("Sqlite implementation was chosen");
 }
 
 module.exports = Database;
